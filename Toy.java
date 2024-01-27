95777f46d95e3736ffdef347c7607dad249fea8b
Dmitrii Nikulin <skygift.work@gmail.com>
2024-01-27 14:44:06 +0300

    Creating Toy class

---
+++ Toy.java
@@ -0,0 +1,15 @@
+package attestation_work_java;
+
+public class Toy {
+    public Integer id;
+    public String name;
+    public Integer count;
+    public double percent;
+    
+    public Toy(Integer id, String name, Integer count, double percent) {
+        this.id = id;
+        this.name = name;
+        this.count = count;
+        this.percent = percent;
+    }
+}
